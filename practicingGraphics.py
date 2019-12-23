ea2c366074bfdbeb6c4cbaa6781eeeb50f9f4831
Justin Reid <17056261+justingabrielreid@users.noreply.github.com>
2019-12-22 16:19:31 -0800

    Lots of Practice with Udemy Python.

---
+++ practicingGraphics.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+#Practicing Graphical Output from the Python Programming Textbook
+
+#import from the graphics module provided
+from graphics import *
+
+#This opens a graphics window
+win = GraphWin('Shapes')
+
